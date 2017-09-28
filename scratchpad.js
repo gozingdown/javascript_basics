8d841bbd9e002b7ca52d2d64d05457d07f87f63f
Zheng Gong <zgong@vistaprint.com>
2017-09-28 16:05:51 -0400

    scratch

---
+++ scratchpad.js
@@ -0,0 +1,67 @@
+function sayHello(name, age) {
+  return name;
+}
+console.log(typeof sayHello)
+var returnValue = sayHello('test')
+console.log(returnValue)
+
+// function expression
+var f = function foo() {
+  console.log("Hello");
+}
+f()
+
+// anonymous function expression
+var g = function() {
+  console.log("Hello");
+}
+g()
+
+var myObj = {
+  "testProp" : true
+}
+myObj.myMethod = function() {
+  console.log("Function in object")
+}
+
+myObj.myMethod()
+
+
+var person = {
+  "firstName":"Zheng",
+  "lastName" : "Gong",
+  "getFullName" : function() {
+    return this.firstName + " " + this.lastName
+  },
+  "address" : {
+    "street":"123 street",
+    "city":"JS",
+    "state" : "CA"
+  },
+  "isFromState" : function(state) {
+    return this.address.state === state
+  }
+}
+
+var fullName = person.getFullName()
+console.log(fullName)
+console.log(person.isFromState("CA"))
+
+// function arguments
+var add = function(a, b) { // arguments is not an array, it's an object!!!
+  var sum = 0;
+  for(var i = 0; i < arguments.length; i++) {
+    sum += arguments[i]
+  }
+  return sum
+}
+
+console.log(add(10,30,2,3,4,5))
+
+
+var myArray = [1,2,3,4,5]
+myArray.push(6)
+console.log(myArray)
+myArray.pop()
+console.log(myArray)
+
